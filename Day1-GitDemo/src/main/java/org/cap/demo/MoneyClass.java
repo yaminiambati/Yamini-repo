62e2ec0113c0f59c0af97433d8e39ed741a16309
yambati <yambati@DIN73002999.corp.capgemini.com>
2018-08-06 12:47:26 +0530

    Branching done.

---
+++ Day1-GitDemo/src/main/java/org/cap/demo/MoneyClass.java
@@ -0,0 +1,7 @@
+package org.cap.demo;
+
+public class MoneyClass {
+	public void details() {
+	System.out.println("Welcome");
+	}
+}
---
+++ Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
@@ -0,0 +1,11 @@
+package org.cap.demo;
+
+public class SalesClass {
+	int[] arr = {1,2,3,-89,56};
+	public void show() {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
