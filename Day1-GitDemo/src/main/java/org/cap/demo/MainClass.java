a3345d1517631a60ccce6505e5a607d8b857e74f
yambati <yambati@DIN73002999.corp.capgemini.com>
2018-08-06 10:37:44 +0530

    My First Commit

---
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,13 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		greet();
+	}
+	public static void greet() {
+		System.out.println("Hello");
+	}
+}
