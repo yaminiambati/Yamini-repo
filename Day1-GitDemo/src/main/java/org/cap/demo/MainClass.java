564f49a46190ed5bfa468f35c1d8e1506a6adc41
yambati <yambati@DIN73002999.corp.capgemini.com>
2018-08-06 12:28:27 +0530

    New class Money is added.

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -4,7 +4,11 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		SalesClass class1 = new SalesClass();
+		MoneyClass class2 = new MoneyClass();
 		System.out.println("Hello World");
+		class1.show();
+		class2.details();
 		greet();
 	}
 	public static void greet() {
