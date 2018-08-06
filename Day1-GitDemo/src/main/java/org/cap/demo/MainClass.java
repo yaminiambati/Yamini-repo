4c75f5908fa6b56e5930e8263645a75c4d36b9ec
yambati <yambati@DIN73002999.corp.capgemini.com>
2018-08-06 10:52:51 +0530

    Modified

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -4,10 +4,11 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("Hello World");
 		greet();
 	}
 	public static void greet() {
 		System.out.println("Hello");
+		System.out.println("Welcome");
 	}
 }
