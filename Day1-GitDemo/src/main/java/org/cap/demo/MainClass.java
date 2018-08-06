7c9bd7a954f25e50e313960de678bcf00df9f0e8
yambati <yambati@DIN73002999.corp.capgemini.com>
2018-08-06 12:10:43 +0530

    Added new class

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -4,8 +4,10 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		SalesClass class1 = new SalesClass();
 		System.out.println("Hello World");
 		greet();
+		class1.show();
 	}
 	public static void greet() {
 		System.out.println("Hello");
